f3b538b5db44aee9bf1998bff4e44edf5cd467f3
Jie Yu <yujiefantasy@gmail.com>
2021-05-08 17:14:21 +0200

    modified by Jacky

--- src/main/java/it/polimi/ingsw/connection/WaitingRoom.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.connection;
-
-public class WaitingRoom {
-
-    public WaitingRoom()
-    {
-
-    }
-}
--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.ingsw.controller;
-
-public abstract class Controller {
-    //no need for this class
-}
